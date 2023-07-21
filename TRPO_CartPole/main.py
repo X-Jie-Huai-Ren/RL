9e6f1751f8a26a26de4b851a13e00fa6be753ad9
Jimmy <1920425406@qq.com>
2023-07-21 10:37:38 +0800

    没能完成TRPO算法，一方面是TRPO中的最大化问题，另一方面需要再看一遍PPO

--- TRPO_CartPole/main.py
+++ TRPO_CartPole/main.py
@@ -8,11 +8,13 @@ import gym
 
 class TrainManager:
     
-    def __init__(self, env, agent) -> None:
+    def __init__(self, env, agent, eps, num_episodes=10000) -> None:
         """
         Params:
             env: 环境
             agent: 构建的智能体
+            eps: float, epslion-greedy算法的探索率
+            num_episodes: int, 回合数
         """
         self.env = env
         self.agent = agent
---
+++ TRPO_CartPole/utils.py
@@ -0,0 +1,8 @@
+
+
+from collections import namedtuple
+
+
+
+
+Transition = namedtuple('Transition', ['state', 'action', 'reward', 'naex_state', 'done'])
